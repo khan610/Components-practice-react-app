b318068c6cc4688253a5c60b2400ef158756666a
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-05-17 06:00:38 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { faker } from '@faker-js/faker';
 const App = () => {
   return (
     <div className="ui container comments">
+      {/* Comment 1 */}
       <div className="comment">
         <a href="/" className="avatar">
           <img alt="avatar" src={faker.image.avatar()} />
@@ -12,6 +13,38 @@ const App = () => {
         <div className="content">
           <a href="/" className="author">
             Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+
+      {/* Comment 2 */}
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Khan
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+
+      {/* Comment 3 */}
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Natija
           </a>
           <div className="metadata">
             <span className="date">Today at 6:00PM</span>
